0e9ad5ac28a5098cf3cd4eea724b87eff838035d
CodeCrafter-TL <1825456084@qq.com>
2025-01-11 20:58:51 +0800

    Update

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ const hover = document.querySelector('div[hover]');
 setInterval(() => {
     hover.style.opacity = (hover.style.opacity === '1' ? '0' : '1');
 }, 400);
-
-const text = document.querySelector('p[text]');
-
-text.setAttribute('data-text', text.innerHTML);
