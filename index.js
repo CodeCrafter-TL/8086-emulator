31ad7effb3c1feb86b9dcacbefd809bc6740754d
CodeCrafter-TL <1825456084@qq.com>
2025-01-16 09:45:15 +0800

    Still fixing

--- index.js
+++ index.js
@@ -6,15 +6,42 @@ let temp = null;
 let now_y = 0;
 let count = 0;
 
+function liveServer() {
+    let comments = [];
+    for (let i = 0; i < document.body.childNodes.length; i++) {
+        const node = document.body.childNodes[i];
+
+        if (node.nodeType === Node.COMMENT_NODE) {
+            comments.push(node.nodeValue.trim());
+        }
+    }
+
+    if (comments.includes("Code injected by live-server")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function updateHover() {
     if (temp) {
         temp.remove();
     }
-    if (now_y !== text.clientHeight || !now_y) {
-        now_y = text.clientHeight;
-    }
-    if (count > 0) {
-        now_y = text.clientHeight + 4;
+    if (!liveServer()) {
+        if (now_y !== text.clientHeight || !now_y) {
+            now_y = text.clientHeight;
+        }
+        if (count > 0) {
+            now_y += 4;
+        }
+        count += 1;
+    } else {
+        if (now_y !== text.clientHeight || !now_y) {
+            now_y = text.clientHeight + 4;
+        }
+        // if (count > 0) {
+        //     now_y = text.clientHeight + 4;
+        // }
     }
 
     temp = document.createElement("p");
@@ -22,22 +49,24 @@ function updateHover() {
     temp.setAttribute("temp", "");
     temp.style.opacity = 0;
     temp.style.position = "absolute";
+    document.body.appendChild(temp);
     setTimeout(() => {
         hover.style.opacity = 1;
         hover.style.left = temp.clientWidth + 10 + "px";
         hover.style.top = now_y + 'px';
-    }, 40);
-    document.body.appendChild(temp);
-    count += 1;
+    }, 50);
 }
 
 document.addEventListener("keypress", (event) => {
     let key = event.key;
     if (key === "Enter") {
-        text.innerHTML += "<br>";
+        text.innerHTML += "<br><span style=\"color: black; width: 0.1px;\">1</span>";
     } else if (key === "Backspace") {
         text.innerHTML = text.innerHTML.slice(0, -1);
     } else {
+        if (text.innerHTML.endsWith("<span style=\"color: black; width: 0.1px;\">1</span>")) {
+            text.innerHTML = text.innerHTML.replace("<span style=\"color: black; width: 0.1px;\">1</span>", "");
+        }
         text.innerHTML += key;
     }
     updateHover();
